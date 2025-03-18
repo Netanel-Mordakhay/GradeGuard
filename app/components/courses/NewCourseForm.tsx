bf14e9131c6f7f854c619ac456fa79888bf3df32
Netanel <mordakhay.netanel@gmail.com>
2025-03-18 19:33:40 +0200

    Handle form submission for new course

--- app/components/courses/NewCourseForm.tsx
+++ app/components/courses/NewCourseForm.tsx
@@ -1,17 +1,36 @@
+"use client";
 import { Button, Input, InputWrapper, Stack } from "@mantine/core";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 import React from "react";
 
+interface CourseForm {
+  title: string;
+  grade: number;
+}
+
 const NewCourseForm = () => {
+  const { register, handleSubmit } = useForm<CourseForm>();
   return (
-    <Stack>
-      <InputWrapper label="Course name">
-        <Input placeholder="name" />
-      </InputWrapper>
-      <InputWrapper label="Course grade" description="optional">
-        <Input placeholder="0-100" />
-      </InputWrapper>
-      <Button>Create course</Button>
-    </Stack>
+    <form
+      onSubmit={handleSubmit(
+        async (data) => await axios.post("/api/courses", data)
+      )}
+    >
+      <Stack>
+        <InputWrapper label="Course title">
+          <Input placeholder="title" {...register("title")} />
+        </InputWrapper>
+        <InputWrapper label="Course grade" description="optional">
+          <Input
+            placeholder="0-100"
+            type="number"
+            {...register("grade", { valueAsNumber: true })}
+          />
+        </InputWrapper>
+        <Button type="submit">Create course</Button>
+      </Stack>
+    </form>
   );
 };
 
