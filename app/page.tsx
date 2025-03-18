3ff0f696a2d7588cd1113633aab282299fb6c037
Netanel <mordakhay.netanel@gmail.com>
2025-03-18 16:44:17 +0200

    Prisma setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div
-      style={{
-        backgroundImage: `url("https://149359637.v2.pressablecdn.com/wp-content/uploads/2021/08/Space-Earth-Wallpaper-About-Murals.jpg")`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        width: "50vw",
-        height: "80vh",
-      }}
-    >
-      Hello world
-    </div>
-  );
+  return <div>Hello world</div>;
 }
