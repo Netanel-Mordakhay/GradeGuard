8a318e2e212da8b15969b8b01cfef408c18ea1e1
Netanel <mordakhay.netanel@gmail.com>
2025-03-22 12:02:38 +0200

    DashboardGrid breakpoints change

--- app/components/layout/DashboardGrid.tsx
+++ app/components/layout/DashboardGrid.tsx
@@ -11,13 +11,13 @@ const DashboardGrid = ({ children }: Props) => {
   const [child1] = React.Children.toArray(children);
   return (
     <Grid>
-      <GridCol span={{ base: 12, sm: 3 }}>{child1}</GridCol>
-      <GridCol span={{ base: 12, sm: 4 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, sm: 5 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, sm: 4 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, sm: 3 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, sm: 3 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, sm: 6 }}>{child}</GridCol>
+      <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child1}</GridCol>
+      <GridCol span={{ base: 12, md: 6, xl: 4 }}>{child}</GridCol>
+      <GridCol span={{ base: 12, md: 6, xl: 5 }}>{child}</GridCol>
+      <GridCol span={{ base: 12, md: 6, xl: 4 }}>{child}</GridCol>
+      <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child}</GridCol>
+      <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child}</GridCol>
+      <GridCol span={{ base: 12, md: 6, xl: 6 }}>{child}</GridCol>
     </Grid>
   );
 };
