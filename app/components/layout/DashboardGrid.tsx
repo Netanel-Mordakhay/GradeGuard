f144f7823ab9745779ed4a02eb456fd67de42670
Netanel <mordakhay.netanel@gmail.com>
2025-03-22 16:45:17 +0200

    Dashboard grid stack

--- app/components/layout/DashboardGrid.tsx
+++ app/components/layout/DashboardGrid.tsx
@@ -1,5 +1,6 @@
-import { Box, Grid, GridCol, Skeleton } from "@mantine/core";
+import { Box, Grid, GridCol, Skeleton, Stack } from "@mantine/core";
 import React, { ReactNode } from "react";
+import WelcomeBar from "../dashboard/WelcomeBar";
 
 const child = <Skeleton height={220} radius="md" animate={false} />;
 
@@ -10,15 +11,17 @@ interface Props {
 const DashboardGrid = ({ children }: Props) => {
   const [child1] = React.Children.toArray(children);
   return (
-    <Grid>
-      <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child1}</GridCol>
-      <GridCol span={{ base: 12, md: 6, xl: 4 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, md: 6, xl: 5 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, md: 6, xl: 4 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child}</GridCol>
-      <GridCol span={{ base: 12, md: 6, xl: 6 }}>{child}</GridCol>
-    </Grid>
+    <Stack gap={10}>
+      <Grid>
+        <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child1}</GridCol>
+        <GridCol span={{ base: 12, md: 6, xl: 4 }}>{child}</GridCol>
+        <GridCol span={{ base: 12, md: 6, xl: 5 }}>{child}</GridCol>
+        <GridCol span={{ base: 12, md: 6, xl: 4 }}>{child}</GridCol>
+        <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child}</GridCol>
+        <GridCol span={{ base: 12, md: 6, xl: 3 }}>{child}</GridCol>
+        <GridCol span={{ base: 12, md: 6, xl: 6 }}>{child}</GridCol>
+      </Grid>
+    </Stack>
   );
 };
 
