f1d43264473d89b8628290e9d9e9215b2718128c
Netanel Mordakhay <mordakhay.netanel@gmail.com>
2025-07-09 21:01:11 +0300

    Added root link to logo

--- app/components/global/Logo.tsx
+++ app/components/global/Logo.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Box, Title } from "@mantine/core";
+import Link from "next/link";
 import React from "react";
 
 interface Props {
@@ -9,13 +11,15 @@ const Logo = ({ large }: Props) => {
   const classSize = large ? "big" : "small";
   // 28
   return (
-    <Box className={`logo-container-${classSize}`}>
-      <Title size={large ? 48 : 28}>
-        <span>Grade</span>
-        <span className="blue-filled">Guard</span>
-      </Title>
-      <img src="/images/hat.webp" className={`logo-hat-${classSize}`} />
-    </Box>
+    <Link href="/" style={{ textDecoration: "none", color: "inherit" }}>
+      <Box className={`logo-container-${classSize}`}>
+        <Title size={large ? 48 : 28}>
+          <span>Grade</span>
+          <span className="blue-filled">Guard</span>
+        </Title>
+        <img src="/images/hat.webp" className={`logo-hat-${classSize}`} />
+      </Box>
+    </Link>
   );
 };
 
